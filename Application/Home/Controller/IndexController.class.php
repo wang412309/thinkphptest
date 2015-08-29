94a21fdbd8701eff56c8c7f6fc2a1400065e5c78
Administrator <admin@example>
2015-08-29 12:27:12 +0800

    继续添加一个测试方法

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -10,6 +10,10 @@ class IndexController extends Controller {
     }
     
     public function test1(){
+    	echo '测试控制器';
     	echo '撒大发生地方撒地方制器';
+    }
+    public function test2(){
+    	echo '测试控制器';
     }
 }
