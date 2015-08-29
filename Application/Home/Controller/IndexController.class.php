1ea98f2b19cae907eacc1c5e61f7e93bdcafa695
Administrator <admin@example>
2015-08-29 12:29:00 +0800

    删除一个测试方法

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -5,9 +5,6 @@ class IndexController extends Controller {
     public function index(){
     	echo '首页控制器';
     }
-    public function test(){
-    	echo '测试控制器';
-    }
     
     public function test1(){
     	echo '测试控制器';
